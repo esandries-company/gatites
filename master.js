e7efbc3b7e711ca54df5e9a1c09b43e8df288a21
esandries <emanuelsandries@gmail.com>
2021-06-15 00:43:53 -0600

    se areglo bug dle nav abierto en mobile

--- master.js
+++ master.js
@@ -4,6 +4,8 @@ let menuButton = document.querySelector('.button-menu');
 let navOptions = document.querySelector('.nav_options');
 let heroHeadline =document.querySelector('.heroHeadline');
 let productCard =document.querySelector('.product_card');
+let productCard2 =document.querySelector('.product_card-2');
+
 
 closeButton.addEventListener('click', togglenavOptions);
 closeButton.addEventListener('click', animationHero);
@@ -15,8 +17,10 @@ function togglenavOptions(){
     closeButton.classList.toggle('button-close-show')
     heroHeadline.classList.remove('fadeIn');
     productCard.classList.remove('fadeInUp');
+    productCard2.classList.remove('fadeInUp');
 }
 function animationHero(){
     heroHeadline.classList.toggle('fadeIn');
     productCard.classList.toggle('fadeInUp');
+    productCard2.classList.toggle('fadeInUp');
 }
