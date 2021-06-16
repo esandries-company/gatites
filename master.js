8e9d7130446d4585f65585af7636099c16f56653
esandries <emanuelsandries@gmail.com>
2021-06-16 09:03:29 -0600

    version final

--- master.js
+++ master.js
@@ -5,7 +5,27 @@ let navOptions = document.querySelector('.nav_options');
 let heroHeadline =document.querySelector('.heroHeadline');
 let productCard =document.querySelector('.product_card');
 let productCard2 =document.querySelector('.product_card-2');
+let productImageBuy =document.querySelector('.section--procut__Image');
+let productDescription =document.querySelector('.parrafo-description');
 
+var select = document.getElementById('product-selected');
+select.addEventListener('change',
+  function(){
+    var selectedOption = this.options[select.selectedIndex];
+    console.log(selectedOption.value + ': ' + selectedOption.text);
+    if(selectedOption.text == "Super Pack" ){
+        console.log("arriba españa");
+        productImageBuy.classList.add('super-pack');
+        productImageBuy.classList.remove('starter-pack');
+        productDescription.innerHTML = "Llevate 3 gatitos por el precio de 2. Con los 3 gatitos podras estar siempre con alguien. ";
+    } else{
+        console.log("abjao espña");
+        productImageBuy.classList.add('starter-pack');
+        productImageBuy.classList.remove('super-pack');
+        productDescription.innerHTML = "Miles de productos en el mercado pero nada como una caja con un gatito";
+    }
+  });
+  
 
 closeButton.addEventListener('click', togglenavOptions);
 closeButton.addEventListener('click', animationHero);
@@ -14,7 +34,7 @@ menuButton.addEventListener('click', togglenavOptions);
 
 function togglenavOptions(){
     navOptions.classList.toggle('nav_options--show');
-    closeButton.classList.toggle('button-close-show')
+    closeButton.classList.toggle('button-close-show');
     heroHeadline.classList.remove('fadeIn');
     productCard.classList.remove('fadeInUp');
     productCard2.classList.remove('fadeInUp');
